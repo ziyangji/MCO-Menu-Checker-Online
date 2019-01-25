300261f092b9f641f2370e9eae0a55717e3498e7
Jeremiah C. Shikan <github.shikanchen@gmail.com>
2019-01-25 17:22:18 -0500

    test success

--- App/Home/Controller/IndexController.class.php
+++ App/Home/Controller/IndexController.class.php
@@ -5,6 +5,7 @@ class IndexController extends CommonController
 {
     public function index()
     {
+        $this->assign('content', '<div class="container"><h1>It works!</h1></div>');
         $this->display();
     }
 }
