9803dd2eb5bbbf1278ef925e3e87216a34487647
Jeremiah C. Shikan <github.shikanchen@gmail.com>
2019-01-29 15:33:25 -0500

    debug menu template

--- App/Home/Controller/IndexController.class.php
+++ App/Home/Controller/IndexController.class.php
@@ -6,7 +6,11 @@ class IndexController extends CommonController
     public function index()
     {
         $this->assign('content', '<div class="container"><h1>It works!</h1></div>');
-        $this->display();
+        
+//        menu template debug
+        $this->redirect('Index/menu', 'loading menu...');
+
+//        $this->display();
     }
     
     public function menu()
