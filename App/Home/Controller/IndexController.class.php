c78853ef5a1e97158ca6df47227921eb245091ff
Jeremiah C. Shikan <github.shikanchen@gmail.com>
2019-01-29 15:25:31 -0500

    menu template added

--- App/Home/Controller/IndexController.class.php
+++ App/Home/Controller/IndexController.class.php
@@ -8,4 +8,9 @@ class IndexController extends CommonController
         $this->assign('content', '<div class="container"><h1>It works!</h1></div>');
         $this->display();
     }
+    
+    public function menu()
+    {
+        $this->display();
+    }
 }
