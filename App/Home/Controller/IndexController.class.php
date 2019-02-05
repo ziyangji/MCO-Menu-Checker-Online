62a9b6eba7201591baf81364d40b2d96a7c1684f
Jeremiah C. Shikan <github.shikanchen@gmail.com>
2019-02-05 16:49:13 -0500

    data compress

--- App/Home/Controller/IndexController.class.php
+++ App/Home/Controller/IndexController.class.php
@@ -15,6 +15,28 @@ class IndexController extends CommonController
     
     public function menu()
     {
+        
+        $dininghall = array(
+            'cms' => array(
+                'id' => 0,
+                'name' => "Commons Dining Hall"
+            ),
+            'barh' => array(
+                'id' => 1,
+                'name' => "Barh Dining Hall"
+            ),
+            'sage' => array(
+                'id' => 2,
+                'name' => "Sage Dining Hall"
+            ),
+            'blm' => array(
+                'id' => 3,
+                'name' => "Sage Dining Hall"
+            ),
+        );
+
+        $this->assign('data', $dininghall);
+        
         $this->display();
     }
 }
