2fd2671e46d6efbabbe6e7e1414a415dbac02968
Jeremiah C. Shikan <github.shikanchen@gmail.com>
2019-02-20 20:54:41 -0500

    security check

--- App/Home/Controller/CommonController.class.php
+++ App/Home/Controller/CommonController.class.php
@@ -6,5 +6,10 @@ class CommonController extends Controller
 	public function _initialize()
 	{
 		header('Content-Type:text/html;charset=utf-8');
+		if( !session('uid') ) {
+//			TODO: prevent user data leaking
+//			$this->redirect("Admin/Login/login");
+//			exit;
+		}
 	}
 }
