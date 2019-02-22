6cae3f86b1d89a2186f601d9ea43364def40c296
Sun Yutao <sunyu.tt@163.com>
2019-02-22 16:18:28 -0500

    basic Database Class.

---
+++ App/Home/Model/Database.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sun Yutao
+ * Date: 2019/2/22
+ * Time: 16:11
+ */
+
+class Database
+{
+    private $ip;
+    private $port;
+
+    public function addPic()
+    {
+        pass;
+    }
+
+    public function getPic()
+    {
+        pass;
+    }
+
+}
