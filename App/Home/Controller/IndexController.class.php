8672f5b312a0218e046a1f354fde5ba9d635954f
Jeremiah C. Shikan <github.shikanchen@gmail.com>
2019-02-28 00:28:07 -0500

    index page refresh

--- App/Home/Controller/IndexController.class.php
+++ App/Home/Controller/IndexController.class.php
@@ -6,7 +6,7 @@ class IndexController extends CommonController
     public function index()
     {
         $content = array(
-            0 => 'MCO Online Menu Checker',
+            0 => 'Menu Checker',
             1 => '\tAn online preview of the RPI dining hall menus.\n\tStudents and dinning hall staffs will be able to upload both text descriptions and pictures of dishes provided by each of the dinning halls.\n\tThe menus will be updated "mealy" by users, and we are considering to collaborate with the dinning halls to make it easier.'
         );
         
