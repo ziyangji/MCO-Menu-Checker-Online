9e1e173cb2d018410667725ec8643641da7076d0
Sitong Liu <liusitong1999@outlook.com>
2019-03-12 14:39:20 -0400

    Update data_fetch_test_version.py

--- App/Common/Common/data_fetch_test_version.py
+++ App/Common/Common/data_fetch_test_version.py
@@ -29,3 +29,36 @@ for index,item in enumerate(items):
 			file.flush()
 		file.close()  
 		# close file
+		
+//
+version 2:
+import re
+import requests
+
+
+
+def dowmloadPic(html,keyword):
+
+
+    pic_url = re.findall('"objURL":"(.*?)",',html,re.S)
+    i = 0
+    for each in pic_url:
+        try:
+            pic= requests.get(each, timeout=10)
+        except requests.exceptions.ConnectionError:
+            print 'Unable to download'
+            continue
+        string = 'pictures\\'+keyword+'_'+str(i) + '.jpg'
+        #resolve the problem of encode, make sure that chinese name could be store
+        fp = open(string.decode('utf-8').encode('cp936'),'wb')
+        fp.write(pic.content)
+        fp.close()
+        i += 1
+
+
+
+if __name__ == '__main__':
+    word = raw_input("Input key word: ")
+    url = 'https://menus.sodexomyway.com/BiteMenu/Menu?menuId=15288&locationId=76929015&whereami=http://rensselaerdining.com/dining-near-me/blitman-dining-hall'
+    result = requests.get(url)
+    dowmloadPic(result.text,word)//
