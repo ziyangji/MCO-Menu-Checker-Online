7815d537a3f7556ae4a63174870f17843bbad63d
Jeremiah C. Shikan <github.shikanchen@gmail.com>
2019-04-17 21:59:42 -0400

    clean script && add clean.sh for trash_code deletion

---
+++ App/Common/Common/clean.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm ../../Home/index.html
+rm ../../Home/Common/index.html
+rm ../../Home/Conf/index.html
+rm ../../Home/Controller/index.html
+rm ../../Home/Model/index.html
+rm ../../Home/View/index.html
+
+touch ../../Home/index.html
+touch ../../Home/Common/index.html
+touch ../../Home/Conf/index.html
+touch ../../Home/Controller/index.html
+touch ../../Home/Model/index.html
+touch ../../Home/View/index.html
+
