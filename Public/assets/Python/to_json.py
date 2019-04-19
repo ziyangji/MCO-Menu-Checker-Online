60976b3ee18dbe1198493561f11f47b437a33afd
ruizhe <lir7@rpi.edu>
2019-04-19 15:15:56 -0400

    to_json file test
    add some tests

--- Public/assets/Python/to_json.py
+++ Public/assets/Python/to_json.py
@@ -1,5 +1,12 @@
 import json
 
+# expect "names" to be a list
+# expect "cals" to be a list
+# expect names have the same length as cals
+# If names and cals's length are different, raise exception
+#
+# Return a dictionary contains {names1, cals1}, {names2, cals2}....
+#
 
 def to_dict(names, cals):
     if len(names) != len(cals):
@@ -9,4 +16,34 @@ def to_dict(names, cals):
     for i in range(len(names)):
         menu[names[i]] = cals[i]
     return menu
+
+
+if __name__ == '__main__':
+    
+    #empty
+    names = []
+    cals = []
+    menu = to_dict(names, cals)
+    print(menu)
+    
+    
+    '''
+    #should raise runtime exception
+    names = ["1"]
+    cals = []
+    menu = to_dict(names, cals)
+    '''
+    
+    #regular case with one input
+    names = ["1"]
+    cals = ["1"]
+    menu = to_dict(names, cals)
+    print(menu)    
+    
+    #regular case with small number of inputs
+    names = ["a", "b", "c", "d", "e"]
+    cals = ["1", "2", "3", "4", "5"]
+    menu = to_dict(names, cals)
+    print(menu)   
+
 
